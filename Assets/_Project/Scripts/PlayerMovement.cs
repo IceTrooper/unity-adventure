a4a528f9f7af8aeb2d4963ff05016b7dec4206e9
Ice_trooper <trooperice@gmail.com>
2021-03-14 21:35:08 +0100

    added player movement; added animations to movement

--- Assets/_Project/Scripts/PlayerMovement.cs
+++ Assets/_Project/Scripts/PlayerMovement.cs
@@ -3,30 +3,40 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField, Range(0.5f, 10f)] private float movementSpeed = 5f;
-    [SerializeField, Range(15f, 180f)] private float rotationSpeed = 60f;
-    [SerializeField, Range(1f, 5f)] private float jumpForceValue = 2f;
-    private Vector3 movementDirection;
-    private Vector3 relativeMovementDirection;
+    [SerializeField, Range(0.5f, 10f)] private float maxForwardSpeed = 5f;
+    [SerializeField, Range(0.5f, 10f)] private float maxBackwardSpeed = 2f;
+    [SerializeField, Range(10f, 180f)] private float rotationSpeed = 60f;
+    [SerializeField, Range(1f, 20f)] private float jumpForceValue = 2f;
+    [SerializeField] private float groundAcceleration = 20f;
+    [SerializeField] private float groundDeceleration = 25f;
+
+    private Vector2 inputDirection;
     private Vector3 playerVelocity;
+
+    // References
     private CharacterController characterController;
+    private Animator animator;
+
+    // Animation hashes
+    private int hashMoveSpeed = Animator.StringToHash("MoveSpeed");
+    private int hashIsGrounded = Animator.StringToHash("IsGrounded");
+    private int hashJump = Animator.StringToHash("Jump");
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
         Move();
-        Rotate();
+        animator.SetBool(hashIsGrounded, characterController.isGrounded);
     }
 
     public void OnMoveInput(InputAction.CallbackContext context)
     {
-        var inputMovement = context.ReadValue<Vector2>();
-        movementDirection = new Vector3(inputMovement.x, movementDirection.y, inputMovement.y);
-        //relativeMovementDirection = transform.TransformDirection(movementDirection)
+        inputDirection = context.ReadValue<Vector2>();
     }
 
     public void OnJumpInput(InputAction.CallbackContext context)
@@ -34,28 +44,38 @@ public class PlayerMovement : MonoBehaviour
         if(context.started && characterController.isGrounded)
         {
             playerVelocity.y += Mathf.Sqrt(-jumpForceValue * Physics.gravity.y);
+            animator.SetTrigger(hashJump);
         }
     }
 
     private void Move()
     {
+        // Check if on ground and reset velocity on Y axis.
         if(characterController.isGrounded && playerVelocity.y < 0f)
         {
             playerVelocity.y = 0f;
         }
 
-        if(movementDirection != Vector3.zero)
+        // Apply gravity
+        playerVelocity += Physics.gravity * Time.deltaTime;
+
+        // Calculate desired velocity based on forward/back input movement.
+        float desiredVelocityZ = inputDirection.y == 1f ? maxForwardSpeed : maxBackwardSpeed;
+        // Multiply by direction
+        desiredVelocityZ *= inputDirection.y;
+
+        // Calculate acceleration (if player is pressing the movement button).
+        float acceleration = inputDirection.y != 0f ? groundAcceleration : groundDeceleration;
+        playerVelocity.z = Mathf.MoveTowards(playerVelocity.z, desiredVelocityZ, acceleration * Time.deltaTime);
+
+        // Check if there is rotation in place and apply slight forward movement.
+        if(inputDirection.y == 0f && inputDirection.x != 0f && playerVelocity.z < 1.5f)
         {
-            var relativeDirection = transform.TransformDirection(movementDirection);
-            characterController.Move(relativeDirection * (movementSpeed * Time.deltaTime));
+            playerVelocity.z = 1.5f;
         }
 
-        playerVelocity += Physics.gravity * Time.deltaTime;
-        characterController.Move(playerVelocity * Time.deltaTime);
-    }
-
-    private void Rotate()
-    {
-        //var newRotation = Quaternion.LookRotation()
+        characterController.Move(transform.TransformDirection(playerVelocity) * Time.deltaTime);
+        transform.Rotate(Vector3.up * inputDirection.x * rotationSpeed * Time.deltaTime);
+        animator.SetFloat(hashMoveSpeed, playerVelocity.z);        
     }
 }

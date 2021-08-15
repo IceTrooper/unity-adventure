6616f522281081bcf2481bd3c66f6788993b5684
Ice_trooper <trooperice@gmail.com>
2021-08-15 14:31:46 +0200

    added restarting the level after touching the main crystal

---
+++ Assets/_Project/Scripts/Crystal.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Crystal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}

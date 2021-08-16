fc6d3a6c586b20abe83aea57c46c37aad1a70430
Ice_trooper <trooperice@gmail.com>
2021-08-16 23:03:41 +0200

    added game informations on GUI; added TextMeshPro

---
+++ Assets/_Project/Scripts/InformationsPanel_UI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InformationsPanel_UI : MonoBehaviour
+{
+    [SerializeField] private GameObject informationsPanel;
+
+    private void Update()
+    {
+        if(Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            informationsPanel.SetActive(!informationsPanel.activeSelf);
+        }
+    }
+}
